fae17c62082ec23bf8d7823d753209db49b42ac0
trung97 <1513752@hcmut.edu.vn>
2018-03-21 16:24:54 +0700

    thay app setup dang bi loi chua doi modules

---
+++ AwesomeProject/App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Setup from "./src/boot/setup";
+export default class App extends React.Component{
+  render(){
+    return <Setup />
+  }
+}
+
+
